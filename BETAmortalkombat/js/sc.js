d6ba7c890e86019c7c32a772baa5351327b45e01
Latteo <98646683+ricoaxmerov@users.noreply.github.com>
2022-09-21 16:18:31 +0400

    Add files via upload

---
+++ BETAmortalkombat/js/sc.js
@@ -0,0 +1,108 @@
+
+let pers12 = document.getElementById("pers12")
+let pers22 = document.getElementById("pers22")
+let pers32 = document.getElementById("pers32")
+let pers42 = document.getElementById("pers42")
+let pers52 = document.getElementById("pers52")
+let pers62 = document.getElementById("pers62")
+let pers72 = document.getElementById("pers72")
+let pers82 = document.getElementById("pers82")
+let perselect = document.getElementById("pers-select")
+
+
+
+function pr1(){
+	pers12.classList.add("active")
+	pers22.classList.remove("active")
+	pers32.classList.remove("active")
+	pers42.classList.remove("active")
+	pers52.classList.remove("active")
+	pers62.classList.remove("active")
+	pers72.classList.remove("active")
+	pers82.classList.remove("active")
+	perselect.src = "img/pers1.gif"
+}
+  
+function pr2(){
+	pers22.classList.add("active")
+	pers12.classList.remove("active")
+	pers32.classList.remove("active")
+	pers42.classList.remove("active")
+	pers52.classList.remove("active")
+	pers62.classList.remove("active")
+	pers72.classList.remove("active")
+	pers82.classList.remove("active")
+	perselect.src = "img/pers2.gif"
+}
+  
+function pr3(){
+	pers32.classList.add("active")
+	pers12.classList.remove("active")
+	pers22.classList.remove("active")
+	pers42.classList.remove("active")
+	pers52.classList.remove("active")
+	pers62.classList.remove("active")
+	pers72.classList.remove("active")
+	pers82.classList.remove("active")
+	perselect.src = "img/pers3.gif"
+}
+function pr4(){
+	pers42.classList.add("active")
+	pers12.classList.remove("active")
+	pers22.classList.remove("active")
+	pers32.classList.remove("active")
+	pers52.classList.remove("active")
+	pers62.classList.remove("active")
+	pers72.classList.remove("active")
+	pers82.classList.remove("active")
+	perselect.src = "img/pers4.gif"
+}
+function pr5(){
+	pers52.classList.add("active")
+	pers12.classList.remove("active")
+	pers22.classList.remove("active")
+	pers32.classList.remove("active")
+	pers42.classList.remove("active")
+	pers62.classList.remove("active")
+	pers72.classList.remove("active")
+	pers82.classList.remove("active")
+	perselect.src = "img/pers5.gif"
+}
+
+function pr6(){
+	pers52.classList.add("active")
+	pers12.classList.remove("active")
+	pers22.classList.remove("active")
+	pers32.classList.remove("active")
+	pers42.classList.remove("active")
+	pers62.classList.remove("active")
+	pers72.classList.remove("active")
+	pers82.classList.remove("active")
+	perselect.src = "img/pers6.gif"
+}
+  
+function pr7(){
+	pers52.classList.add("active")
+	pers12.classList.remove("active")
+	pers22.classList.remove("active")
+	pers32.classList.remove("active")
+	pers42.classList.remove("active")
+	pers62.classList.remove("active")
+	pers72.classList.remove("active")
+	pers82.classList.remove("active")
+	perselect.src = "img/pers7.gif"
+}
+  
+  
+function pr8(){
+	pers52.classList.add("active")
+	pers12.classList.remove("active")
+	pers22.classList.remove("active")
+	pers32.classList.remove("active")
+	pers42.classList.remove("active")
+	pers62.classList.remove("active")
+	pers72.classList.remove("active")
+	pers82.classList.remove("active")
+	perselect.src = "img/pers8.gif"
+}
+  
